fe593f7a3577dc9f938d2a1f145e5cb581eb1cbf
Rinkesh Nagmoti <nagmoti.rinkesh@gmail.com>
2009-06-26 21:23:14 +0000

    Added a new class.

---
+++ JMLUNITNG/src/org/jmlspecs/jmlunitng/JMLUNITNGTestClassGenerator.java
@@ -0,0 +1,12 @@
+package org.jmlspecs.jmlunitng;
+
+/**
+ * Generates the JMLUNITNG_Test class by JMLUNITNG framework.
+ * The generated class runs unit tests for the class to be
+ * tested using JMLUNITNG testing framework.
+ * @author Rinkesh Nagmoti
+ * @version 1.0
+ */
+public class JMLUNITNGTestClassGenerator {
+
+}
