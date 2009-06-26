a9d30478b581f18281a498641166fc0bb9d08580
Rinkesh Nagmoti <nagmoti.rinkesh@gmail.com>
2009-06-26 20:42:11 +0000

    Updated Writer class with Checkstyle.

--- JMLUNITNG/src/org/jmlspecs/jmlunitng/Writer.java
+++ JMLUNITNG/src/org/jmlspecs/jmlunitng/Writer.java
@@ -3,35 +3,42 @@ package org.jmlspecs.jmlunitng;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
+/** Constructs a Writer Object.*/
 public class Writer {
- 
-	/** Constructs a Writer object for java code */
-	public Writer(String fileName) throws FileNotFoundException
-	{
+	/** Constructs a Writer object for java code.
+	 * @param fileName String != null;
+	 * @throws FileNotFoundException;
+	 */
+	public Writer(String fileName) throws FileNotFoundException {
 		file = "C:\\rinkesh.java";
 		p = new PrintWriter(file);
 	}
-	
-	/** Prints the string to the file 
-	 *  @param s String which will be printed in the file
+
+	/** Prints the string to the file.
+	 *  @param s String which will be printed in the file.
 	 */
-	public void print(String s){
-		
+	protected final void print(String s) {
 		p.print(s);
 		p.flush();
 	}
 	/**
 	 * Print a newline.
 	 */
-	public void newLine(){
+	protected final void newLine() {
 		p.print("\n");
 	}
-	 // ----------------------------------------------------------------------
+	/**
+	 * Indent a tab.
+	 */
+	protected final void tab() {
+		p.print("   ");
+	}
+	 // ---------------------------------------------------------------
     // PRIVATE DATA MEMBERS
-    // ----------------------------------------------------------------------
+    // ----------------------------------------------------------------
 
+	/**PrintWriter object to write string to the file.*/
 	private PrintWriter p;
-	private String file ;
-	
-
+	/**String to mention the file name for the class.*/
+	private String file;
 }
