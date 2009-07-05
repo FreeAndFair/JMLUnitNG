f21881ec1357888fa538fc267e672d930da5ba83
Rinkesh Nagmoti <nagmoti.rinkesh@gmail.com>
2009-07-05 17:00:04 +0000

    Updated main class for test generation.

--- JMLUNITNG/src/org/jmlspecs/jmlunitng/MainClass.java
+++ JMLUNITNG/src/org/jmlspecs/jmlunitng/MainClass.java
@@ -1,39 +1,12 @@
 package org.jmlspecs.jmlunitng;
 
-import java.io.BufferedReader;
+
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-
 import java.util.Iterator;
-import java.util.List;
-
-import javax.print.attribute.standard.Destination;
-
 import org.jmlspecs.jmlunit.JntOptions;
-import org.multijava.mjc.CMethod;
 import org.multijava.mjc.JCompilationUnit;
-import org.multijava.mjc.JCompilationUnitType;
 import org.multijava.mjc.JTypeDeclarationType;
-import org.multijava.mjc.JavadocLexer;
-import org.multijava.mjc.Main;
 import org.multijava.mjc.MjcCommonOptions;
-import org.multijava.mjc.MjcLexer;
-import org.multijava.mjc.MjcMessages;
-import org.multijava.mjc.MjcOptions;
-import org.multijava.mjc.MjcParser;
-import org.multijava.mjc.ParsingController;
-import org.multijava.mjc.ParsingController.ConfigurationException;
-import org.multijava.mjc.ParsingController.KeyException;
-import org.multijava.util.FormattedException;
-import org.multijava.util.compiler.CompilerMessages;
-
-import com.sun.org.apache.xml.internal.utils.URI;
-
-import antlr.RecognitionException;
-import antlr.TokenStreamException;
 
 
 /**
@@ -43,12 +16,12 @@ import antlr.TokenStreamException;
  * @version 1.0
  * Some of the code is taken from MultiJava open source project.
  */
-public class MainClass extends Main
+public class MainClass
 {
   /**
    * MjcComminOptions instance to parse the given file.
    */
-  private  MjcCommonOptions options;
+  protected MjcCommonOptions options;
   
  
   /**
