76a3a9aa1ef497bc0cfef640565b2eca7cf18cdf
Rinkesh Nagmoti <nagmoti.rinkesh@gmail.com>
2009-08-14 21:33:00 +0000

    added class StringArrayStrategy

---
+++ JMLUNITNG/src/org/jmlspecs/jmlunitng/StringArrayStrategy.java
@@ -0,0 +1,10 @@
+package org.jmlspecs.jmlunitng;
+/**
+ *  This class provides the iterator strategy for String Array.
+ * @author Rinkesh Nagmoti.
+ * @version 1.0
+ */
+public class StringArrayStrategy
+{
+
+}
