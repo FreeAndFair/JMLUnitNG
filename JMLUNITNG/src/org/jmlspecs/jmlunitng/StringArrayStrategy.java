4d6bf632ee3c5381685d59af2850aff3ed50a0a2
Rinkesh Nagmoti <nagmoti.rinkesh@gmail.com>
2009-08-17 12:10:30 +0000

    Added method for default data of String array.

--- JMLUNITNG/src/org/jmlspecs/jmlunitng/StringArrayStrategy.java
+++ JMLUNITNG/src/org/jmlspecs/jmlunitng/StringArrayStrategy.java
@@ -1,10 +1,22 @@
 package org.jmlspecs.jmlunitng;
+
+import org.jmlspecs.jmlunit.strategies.ImmutableObjectAbstractStrategy;
+
 /**
  *  This class provides the iterator strategy for String Array.
  * @author Rinkesh Nagmoti.
  * @version 1.0
  */
-public class StringArrayStrategy
+//This method uses the old jmlunit strategies class ImmutableObjectAbstractStrategy
+//to generate iterator.
+public class StringArrayStrategy extends ImmutableObjectAbstractStrategy
 {
-
+  /**
+   * This method provides the default data for iterator.
+   * @return Object[]
+   */
+  protected Object[] defaultData()
+  {
+    return new String[][] {null, {"", ""}, };
+  }
 }
