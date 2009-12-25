9d04a2f0b761279790d2f322283d8895f56e64d8
Daniel M. Zimmerman <dmz@acm.org>
2009-12-25 01:12:52 +0000

    This is a completely working version, I believe.

---
+++ JMLUNITNG/for_testng/src/main/org/testng/ForgetfulTestListenerAdapter.java
@@ -0,0 +1,92 @@
+package org.testng;
+
+import java.util.List;
+
+public class ForgetfulTestListenerAdapter extends TestListenerAdapter {
+
+	private int failed;
+	private int passed;
+	private int skipped;
+	
+	public int failed() { return failed; }
+	public int passed() { return passed; }
+	public int skipped() { return skipped; }
+	
+	@Override
+	public void onConfigurationFailure(ITestResult itr) {
+		// TODO Auto-generated method stub
+		super.onConfigurationFailure(itr);
+	}
+	@Override
+	public void onConfigurationSkip(ITestResult itr) {
+		// TODO Auto-generated method stub
+		super.onConfigurationSkip(itr);
+	}
+	@Override
+	public void onConfigurationSuccess(ITestResult itr) {
+		// TODO Auto-generated method stub
+		super.onConfigurationSuccess(itr);
+	}
+	@Override
+	public void onFinish(ITestContext testContext) {
+		// TODO Auto-generated method stub
+		super.onFinish(testContext);
+	}
+	@Override
+	public void onStart(ITestContext testContext) {
+		// TODO Auto-generated method stub
+		super.onStart(testContext);
+	}
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		super.onTestStart(result);
+	}
+	@Override
+	public void setAllTestMethods(List<ITestNGMethod> allTestMethods) {
+		// TODO Auto-generated method stub
+		super.setAllTestMethods(allTestMethods);
+	}
+	@Override
+	public void setFailedButWithinSuccessPercentageTests(
+			List<ITestResult> failedButWithinSuccessPercentageTests) {
+		// TODO Auto-generated method stub
+		super
+				.setFailedButWithinSuccessPercentageTests(failedButWithinSuccessPercentageTests);
+	}
+	@Override
+	public void setFailedTests(List<ITestResult> failedTests) {
+		// TODO Auto-generated method stub
+		super.setFailedTests(failedTests);
+	}
+	@Override
+	public void setPassedTests(List<ITestResult> passedTests) {
+		// TODO Auto-generated method stub
+		super.setPassedTests(passedTests);
+	}
+	@Override
+	public void setSkippedTests(List<ITestResult> skippedTests) {
+		// TODO Auto-generated method stub
+		super.setSkippedTests(skippedTests);
+	}
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult tr) {
+		failed++;
+	}
+
+	@Override
+	public void onTestFailure(ITestResult tr) {
+		failed++;
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult tr) {
+		skipped++;
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult tr) {
+		passed++;
+	}
+
+}
