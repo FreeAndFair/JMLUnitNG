cf32931ba953e9a3d52f5c0a62c423cf97fce6ad
Daniel M. Zimmerman <dmz@acm.org>
2010-01-25 20:35:56 +0000

    The listener class used in the experiments (committed fairly late in the game).

--- JMLUNITNG/src/org/jmlspecs/jmlunitng/ShortFormTestListener.java
+++ JMLUNITNG/src/org/jmlspecs/jmlunitng/ShortFormTestListener.java
@@ -9,14 +9,20 @@ import org.testng.ITestResult;
  */
 public class ShortFormTestListener extends TestListenerAdapter {
   private int m_count = 0;
- 
+  private int failures = 0;
+  private int skips = 0;
+  
+  private final long startTime = System.currentTimeMillis();
+  
   @Override
   public void onTestFailure(ITestResult tr) {
+    failures++;
     log("F");
   }
  
   @Override
   public void onTestSkipped(ITestResult tr) {
+    skips++;
     log("S");
   }
  
@@ -29,7 +35,9 @@ public class ShortFormTestListener extends TestListenerAdapter {
     boolean new_line = m_count++ % 40 == 0;
     
     if ((m_count - 1) % 100000 == 0 && m_count != 1) {
-      System.out.println("\n" + (m_count - 1) + " tests completed" + "\n");
+      System.out.println("\n" + (m_count - 1) + "/" + skips + "/" + failures + " " +
+                         "tests/skips/fails" + ", elapsed time " +
+                         (System.currentTimeMillis() - startTime) / 1000 + "s" + "\n");
     }
     else if (new_line) {
       System.out.println("");
